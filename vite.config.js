7b09b5b55985036d013d30e653288f0fdd692d95
BlvckKryptonite <mumathedeveloper@gmail.com>
2025-07-31 20:33:59 +0200

    Add SPA rewrite so react-router-dom routes don’t 404

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
-export default defineConfig({
-  base: "/mumas-mazing-barbeque/", // <-- GitHub Pages deployment
-  plugins: [react()],
+export default defineConfig(() => {
+  const isVercel = process.env.VERCEL === '1';
+  return {
+    base: isVercel ? '/' : '/mumas-mazing-barbeque/',
+    plugins: [react(), viteCompression()],
+    build: {
+      sourcemap: false,
+    },
+  };
 });
